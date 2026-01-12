3fd59a9cfcde612b3b428414243e4b728f8a9ceb
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2026-01-12 16:25:17 +1300

    feat: update window design

--- src/UiBuilderLibrary/Uis/Window.cs
+++ src/UiBuilderLibrary/Uis/Window.cs
@@ -13,28 +13,48 @@ public partial class UiBuilderLibrary
         /// <summary>
         /// The top level elements of a window.
         /// </summary>
-        public readonly record struct WindowData(PanelElement Main, PanelElement TitleBar);
+        public readonly record struct WindowComponents(
+            /// <summary>
+            /// The main panel of the window.
+            /// </summary>
+            ///
+            PanelElement MainPanel,
+            /// <summary>
+            /// The title bar of the window.
+            /// </summary>
+            PanelElement TitleBar,
+
+            /// <summary>
+            /// The label displayed in the title bar.
+            /// </summary>
+            LabelElement WindowLabel,
+
+            /// <summary>
+            /// The divider between the title bar and the main panel.
+            /// </summary>
+            PanelElement Divider,
+
+            /// <summary>
+            /// The close button of the window.
+            /// </summary>
+            ButtonElement CloseButton,
+
+            /// <summary>
+            /// The icon displayed on the close button.
+            /// </summary>
+            ImageElement CloseButtonIcon
+        );
 
         /// <summary>
-        /// The data for this window that will be stared to initializers.
+        /// The components that make up a window
         /// </summary>
-        protected readonly WindowData Data;
-
-        /// <summary>
-        /// The main panel of this window.
-        /// </summary>
-        protected PanelElement Main => Data.Main;
-
-        /// <summary>
-        /// The title bar of this window.
-        /// </summary>
-        protected PanelElement TitleBar => Data.TitleBar;
+        protected readonly WindowComponents Components;
 
         /// <summary>
         /// Create a new UI window on the "Overlay" layer.
         /// </summary>
         /// <param name="initializer">Defines what makes up the window.</param>
-        public Window(Action<WindowData> initializer) : this("Overlay", initializer)
+        public Window(Action<WindowComponents> initializer) : this("Overlay", initializer)
         {
         }
 
@@ -43,16 +63,14 @@ public partial class UiBuilderLibrary
         /// </summary>
         /// <param name="layer">The layer to put this UI on.</param>
         /// <param name="initializer">Defines what makes up the window.</param>
-        public Window(string layer, Action<WindowData> initializer) : base(layer)
+        public Window(string layer, Action<WindowComponents> initializer) : base(layer)
         {
-            Data = new WindowData(
-                new PanelElement(Root) { Name = "main" },
-                new PanelElement(Root) { Name = "titleBar", Weight = -1 }
-            );
+            var main = new PanelElement(Root) { Name = "main" };
+            var titleBar = new PanelElement(Root) { Name = "titleBar", Weight = -1 };
 
             Root.Name = "window";
-            Root.Margin.SetSize(Size.Pixels(32));
-            Root.Border.SetSize(Size.Pixels(4));
+            Root.Margin.SetSize(Size.Pixels(24));
+            Root.Border.SetSize(Size.Pixels(2));
             Root.Layout = new FlexLayout(Root)
             {
                 Direction = FlexLayout.FlexDirection.Vertical,
@@ -60,34 +78,72 @@ public partial class UiBuilderLibrary
                 JustifyContent = FlexLayout.JustifyAlignment.Start,
             };
 
-            TitleBar.Height = Size.Pixels(32);
-            TitleBar.BgColor = new Color(0f, 0f, 0f, 0.8f);
-            TitleBar.Layout = new FlexLayout(TitleBar)
+            titleBar.Height = Size.Pixels(30);
+            titleBar.BgColor = ColorPallete.OverlayDarkenLevel1;
+            titleBar.Layout = new FlexLayout(titleBar)
             {
                 Direction = FlexLayout.FlexDirection.Horizontal,
                 AlignItems = FlexLayout.ItemAlignment.Stretch,
-                JustifyContent = FlexLayout.JustifyAlignment.End,
+                JustifyContent = FlexLayout.JustifyAlignment.Start,
             };
 
-            // ReSharper disable once UnusedVariable
+            var windowLabel = new LabelElement(titleBar)
+            {
+                Name = "window-title-bar-label",
+                Text = "Window Title",
+                Font = Font.Bold,
+                FontSize = Size.Pixels(20),
+                TextColor = ColorPallete.TextRegular,
+                TextAlignment = TextAnchor.MiddleLeft,
+                Margin =
+                {
+                    Top = Size.Pixels(0),
+                    Right = Size.Pixels(8),
+                    Bottom = Size.Pixels(0),
+                    Left = Size.Pixels(8)
+                }
+            };
+
+            var closeButton = new ButtonElement(titleBar)
+            {
+                Name = "window-close-button",
+                BgColor = ColorPallete.Red,
+                Width = Size.Pixels(64),
+                Weight = 100,
+            };
+            closeButton.Layout = new FlexLayout(closeButton)
+            {
+                Direction = FlexLayout.FlexDirection.Horizontal,
+                AlignItems = FlexLayout.ItemAlignment.Center,
+                JustifyContent = FlexLayout.JustifyAlignment.Center,
+            };
+
+            var closeButtonIcon = new ImageElement(closeButton)
+            {
+                Name = "window-close-button-icon",
+                Color = ColorPallete.OnRed,
+                Sprite = "assets/icons/close.png",
+                Width = Size.Pixels(14),
+                Height = Size.Pixels(14),
+            };
+
             var divider = new PanelElement(Root)
             {
-                Name = "divider",
+                Name = "window-divider",
                 Weight = -0.5,
-                BgColor = Color.black,
+                BgColor = ColorPallete.Border,
                 Height = Size.Pixels(1),
             };
 
-            // TODO: Make this a button.
-            // ReSharper disable once UnusedVariable
-            var closeButton = new PanelElement(TitleBar)
-            {
-                Name = "closeButton",
-                BgColor = Color.red,
-                Width = Size.Pixels(72),
-            };
-
-            initializer(Data);
+            Components = new WindowComponents(
+                MainPanel: main,
+                TitleBar: titleBar,
+                WindowLabel: windowLabel,
+                Divider: divider,
+                CloseButton: closeButton,
+                CloseButtonIcon: closeButtonIcon
+            );
+            initializer(Components);
         }
     }
 }
