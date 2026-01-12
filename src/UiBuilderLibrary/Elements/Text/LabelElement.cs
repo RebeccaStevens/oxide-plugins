d032e6d11d56d58d0bed2a130aed1656a68839d2
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2026-01-12 16:24:30 +1300

    feat: create a label element

---
+++ src/UiBuilderLibrary/Elements/Text/LabelElement.cs
@@ -0,0 +1,70 @@
+using System;
+using UnityEngine;
+
+namespace Oxide.Plugins;
+
+public partial class UiBuilderLibrary
+{
+    /// <summary>
+    /// An label element.
+    /// </summary>
+    public partial class LabelElement : PanelElementBase
+    {
+        /// <summary>
+        /// The text of the label.
+        /// </summary>
+        public string Text;
+
+        /// <inheritdoc cref="PanelElementBase.Color"/>
+        public Color TextColor
+        {
+            get => Color;
+            set => Color = value;
+        }
+
+        /// <summary>
+        /// The font face to use for the label.
+        /// </summary>
+        public Font Font;
+
+        /// <summary>
+        /// The context for the font size of this element.
+        /// </summary>
+        internal readonly SizeContext FontSizeContext;
+
+        public TextAnchor TextAlignment;
+        public VerticalWrapMode VerticalOverflow;
+
+        /// <summary>
+        /// The font size to use for the label.
+        /// </summary>
+        public Size FontSize
+        {
+            get => FontSizeContext.Get();
+            set
+            {
+                if (value is not Size.PixelSize size)
+                    throw new InvalidOperationException(
+                        "Only pixel sizes are currently supported for font sizes.");
+                FontSizeContext.Set(size);
+            }
+        }
+
+        /// <summary>
+        /// Create a new label element.
+        /// </summary>
+        /// <param name="parent">The parent of this element.</param>
+        public LabelElement(Element parent) : base(parent, null)
+        {
+            Text = "[no label]";
+            TextColor = ColorPallete.TextRegular;
+            Font = Font.Regular;
+            FontSizeContext = new SizeContext("FontSize", this, Size.Pixels(14));
+            TextAlignment = TextAnchor.UpperLeft;
+            VerticalOverflow = VerticalWrapMode.Overflow;
+        }
+
+        /// <inheritdoc/>
+        protected override ElementState InitialState(BasePlayer player) => new LabelElementState(this, player);
+    }
+}
---
+++ src/UiBuilderLibrary/Elements/Text/LabelElementState.cs
@@ -0,0 +1,51 @@
+using Oxide.Game.Rust.Cui;
+
+namespace Oxide.Plugins;
+
+public partial class UiBuilderLibrary
+{
+    public partial class LabelElement
+    {
+        /// <inheritdoc/>
+        public class LabelElementState : PanelElementState
+        {
+            /// <inheritdoc cref="PanelElementState.Element"/>
+            public new LabelElement Element => (LabelElement)base.Element;
+
+            /// <inheritdoc cref="LabelElementState(LabelElement, BasePlayer)"/>
+            public LabelElementState(LabelElement element, BasePlayer player) : base(element, player)
+            {
+            }
+
+            /// <summary>
+            /// Compute the font size to use for this element.
+            /// </summary>
+            protected int ComputeFontSize()
+            {
+                var sizeValue = Element.FontSizeContext.GetBoundsValue(this, CuiBounds.Height);
+                Debug.Assert(sizeValue.Absolute > 0);
+                Debug.Assert(sizeValue.Relative == 0);
+                return (int)sizeValue.Absolute;
+            }
+
+            /// <inheritdoc/>
+            protected override void AddCuiComponents(ElementCuiElements cuiElements)
+            {
+                Debug.AssertNotNull(cuiElements.Root);
+
+                cuiElements.Root.AddComponent(new CuiTextComponent
+                {
+                    Text = Element.Text,
+                    Color = ColorToCuiColor(Element.TextColor),
+                    Font = GetFontFile(Element.Font),
+                    FontSize = ComputeFontSize(),
+                    Align = Element.TextAlignment,
+                    VerticalOverflow = Element.VerticalOverflow,
+                });
+
+                if (Element.HasBorder())
+                    cuiElements.Root.AddComponents(Element.Border.ApplyState(this));
+            }
+        }
+    }
+}
