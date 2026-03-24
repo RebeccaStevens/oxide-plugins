aa9311d1405d6adf6e05d384dc264f6d48e52d7b
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2026-03-24 19:22:14 +1300

    feat: get bounding box size content for a rotated element

--- src/UiBuilderLibrary/Sizes/Abstract/SizeContext.cs
+++ src/UiBuilderLibrary/Sizes/Abstract/SizeContext.cs
@@ -108,5 +108,57 @@ public partial class UiBuilderLibrary
                 ? value
                 : size.ApplyState(state, this);
         }
+
+
+        /// <summary>
+        /// Get the bounding box size for the given state.
+        /// </summary>
+        /// <param name="state">The state of the element to get the values for.</param>
+        /// <returns>The rotated size contexts if the rotation is by a multiple of 90 degrees along with a function to get the bound values.</returns>
+        public static (
+            SizeContext? Width,
+            SizeContext? Height,
+            Func<(Bounds.Value Width, Bounds.Value Height), (Bounds.Value Width, Bounds.Value Height)>
+            GetBoundsValues
+            ) GetBoundingBoxSize(ElementState state)
+        {
+            if (RotationContext.RoughlyEqual(state.Element.Rotation, 0) ||
+                RotationContext.RoughlyEqual(state.Element.Rotation, 180))
+                return (
+                    state.Element.WidthContext,
+                    state.Element.HeightContext,
+                    autoValues => (
+                        state.Element.WidthContext.GetBoundsValue(state, autoValues.Width),
+                        state.Element.HeightContext.GetBoundsValue(state, autoValues.Height)
+                    ));
+
+            if (RotationContext.RoughlyEqual(state.Element.Rotation, 90) ||
+                RotationContext.RoughlyEqual(state.Element.Rotation, 270))
+                return (
+                    state.Element.HeightContext,
+                    state.Element.WidthContext,
+                    autoValues => (
+                        state.Element.HeightContext.GetBoundsValue(state, autoValues.Height),
+                        state.Element.WidthContext.GetBoundsValue(state, autoValues.Width)
+                    ));
+
+            return (
+                null,
+                null,
+                autoValues =>
+                {
+                    var absCosRotation = Math.Abs(Math.Cos(state.Element.Rotation * Math.PI / 180));
+                    var absSinRotation = Math.Abs(Math.Sin(state.Element.Rotation * Math.PI / 180));
+
+                    var widthBaseValue = state.Element.WidthContext.GetBoundsValue(state, autoValues.Width);
+                    var heightBaseValue = state.Element.HeightContext.GetBoundsValue(state, autoValues.Height);
+
+                    var widthValue = widthBaseValue * absCosRotation + heightBaseValue * absSinRotation;
+                    var heightValue = widthBaseValue * absSinRotation + heightBaseValue * absCosRotation;
+
+                    return (widthValue, heightValue);
+                }
+            );
+        }
     }
 }
